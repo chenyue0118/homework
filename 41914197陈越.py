29e17d8dc52a442b6671590d4a0185e9a5f09a69
chenyue0118 <chenyue0118@smail.swufe.edu.cn>
2020-09-24 12:22:07 +0800

    homework

---
+++ 41914197陈越.py
@@ -0,0 +1,302 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # HOMEWORK1
+# ## INTRO TO IT
+# ### 1. Introduction
+# **What is IT?** 4'11''
+#  Use of digital technology to store,process data into useful information.
+# About people.
+# 
+# Failure as feedback 8'15''
+# 
+# ### 2. History of Computing
+# punch card
+# 
+# **The path to modern computer.**
+# 
+# *Cryptography* 15'42'' 
+# 
+# *Vedio games* 23'09''
+# 
+# *Open source* 23'49''
+# Anyone could modify and share
+# Linux
+# 
+# *PDAs(personal digital assistance)* 24'29''
+# 
+# ### 3.Digital Logic
+# **Computer Language**
+# 
+# *Binary System(1s and 0s)* 26'48''
+# 8 bits = a byte-store one character 
+# 256 possible values
+# 
+# **Character Encoding 28'54''** 
+# 
+# Assigns our binary values to characters,so that we humans can read them.
+# 
+# *ASCII* 29'50''
+# 
+# *UTF-8* 30'29''  
+# 
+# **Binary 32'04''*
+# 
+# **How to count in Binary 34'00''**
+# 
+# ### 4.Computer Architecture Layer
+# **Abstraction**
+# To take a relatively complex system and simplify it for our use. 
+# 
+# **Computer Architecture outview 40'00''**
+# *Hardware*
+# Make up of the physical component of a computer.
+# 
+# *Operating system*
+# Allows hardware to communicate with the system.
+# 
+# *Software layer*
+# How we as humans interact with our computers.
+# User
+# 
+# ## Hardware
+# ### 1.The Modern Computer
+# *ports*端口 44'18'' 
+# 
+# *CPU* 45'46'' brain of computers
+# 
+# *RAM* 45'58'' short-term memory
+# 
+# *Hard drive* 46'22'' Holds all datas
+# 
+# *Motherboard* body/circulatory system which connect all pieces
+# 
+# *Power supply*
+# 
+# **Programs and Hardware**
+# cheif-CPU peocess the recipes we send him and makes the food
+# The cheif works super fast that he can cook faster than he could read. we took copy of the recipes and put them into the RAM. The RAM store information.
+# 
+# *EDB(External Data Bus)* 50'46''
+# Interconnect the parts of our computer.
+# 
+# *MCC(Memmory Controller Chip)* 53'00'' Bridge between the CPU and RAM. 
+# 
+# *Address bus* 53'29''Connect CPU with MCC
+# ![relations between some commponents](./2.jpg)
+# 
+# 
+# *Cache* 53'52''Save recent and frequent datas. L1,L2,L3
+# 
+# *Clock speed* 55'24''Themaximum number of clock cycles that it can handle in a certain time peiod.
+# 
+# **Module Introduction**
+# 
+# 
+# ### 2.Components
+# **CPU** 57'11'' 
+# *instruction set* 57'21''
+# 
+# Adding,Subtracting,Copying data--functions
+# When select,make sure it's compatible with your motherboard.
+# 
+# Two types of CPU sockets 1.LGA 2.PGA 59'29''
+# 
+# **RAM** 61'17''
+# short-term memory,not permernent 
+# 
+# **Motherboard**
+# The fundation that holds our computer together. Total boss
+# 
+# Chipset-Decides how components talk to each other. Northbridge,Southbridge
+# 
+# Peripherals 65'45''-External devices we connect to our computer.
+# 
+# Expansion slots 65'57''- To increase the functionality of our computer.
+# 
+# **Storage**
+# ![data conversion chart](./3.jpg)
+# *Hard drive* 69'33''
+# 
+# We store all our computer datas on it.
+# 
+# 2 types of Hard drive
+# HDD-more affordable,more prone to damage ,SSD--less risky to loss data, more expensive
+# 
+# **Power supply**
+# *Wattage* 75'43'' The amount of volts and amps that a device needs.
+# 
+# **peripherals**
+# *USB(Universal Serial Bus)* 77'31''
+# 
+# *MB* 78'16''-unit of data storage,Mb/s-a megabit per second,a unit of data transfer rate.
+# 
+# *Type-C connecter* 79'40'' 
+# 
+# *DVI* 80'07''-just for video 
+# 
+# *HDMI* 80'19''-output for video and audio 
+# 
+# ## Operating System
+# ### 1.what is an operating system?
+# 84'05'' The whole package that manages our computer's resources and lets us interact with it.
+# **Components of an Operating System**
+# 2 main parts-Kernal space&User space
+# ![2 main parts of Operating System](./5.jpg)
+# 
+# Important functions of kernal
+# 
+# *File management* 87'28''
+# 
+# *process management* 88'04'' 
+# 
+# *Memory management* 88'41''
+# 
+# *Input&Output management* 88'57''
+# 
+# **Files and File Systems**
+# Window-NTFS
+# 
+# **Process Management**
+# Progress 94'10''
+# 
+# 运行的程序A program that's excuting,like our internet browser or text editor.
+# 
+# Program  94'13''
+# 
+# 应用程序An application that we can run,like Chrome.
+# We can have many process of the same program running at the same time.
+# 
+# **Memory management**
+# Virtual memory 97'15'' 
+# 
+# Comninatiion of hard drive space and RAM that acts like memory that our processes can use.
+# ![Memory management](./6.jpg)
+# 
+# **I/O management**
+# 
+# **Interacting with the OS: User Space**
+# 2 ways to interact: 
+# 
+# Shell & Graphical user interface图形用户界面(GUI)
+# 
+# shell 102'37'':A program that interprets text commands(文本命令）and sends them to the OS to execute.
+# 
+# **Logs**
+# 105'55''Files that record system events on our computer,just like a system's diary.
+# 
+# **The Boot Process**
+# ![The Boot Process](./7.jpg)
+# 
+# ### 2.Installing an Operating System
+# **Choosing an Operating System**
+# 
+# **Virtual Machine**
+# 113'13'' A copy of real machine.
+# Use of physical resources: Memory,CPU,Storage
+# 
+# ## Networking
+# ### 1.What is networking?
+# **Basics of networking**
+# *Internet* 134'07''
+# 
+# Inerconnection of computers around the world. Like a giant spider web which brings all of them together.
+# The physical connection of computers and wires around the world.
+# 
+# *Network*
+# 
+# Inerconnection of computers
+# 
+# *Web*
+# 
+# The information on the Internet.
+# 
+# *Servers* 136'31''
+# 
+# Connect directly to the Internet. Store the websites that we use.
+# 
+# **Networking Hardware**
+#  
+# *Ethernet cable* 141'44''
+# 
+# Let's you physically connect to the network through cable. 
+# 
+# *Router*路由器
+# 
+# Connrct lots of different devices togrther and helps route network traffic.
+# 
+# *Network stack*
+# 
+# A set of hardware or software that provides the infrastructure for a computer.
+# 
+# **The Web**
+# 
+# All websites can be accessed through the web.
+# 
+# www--world wide web    domain name
+# 
+# ### Impact of the Internet
+# 
+# **Internet of Things(IOT)** 155'33''
+# 
+# 
+# ## Software
+# ### 1. Intro to Software
+# 
+# **What is software**
+# 
+# *coding*
+# 
+# Translating one language to another.
+# 
+# *Scripting* 
+# 
+# Coding ina scripting language脚本语言.
+# 
+# *Scripts*
+# 
+# Mainly used to perform a single or limited range task.
+# 
+# **Types of software**
+# 
+# *Application Software* 173'11''
+# 
+# Any software created to fulfill a specific need.(text editor,web browser浏览器，graphic editor)
+# 
+# *System software* 173'21''
+# 
+# Software used to keep our core system running.Like operating system tools and utilities.
+# 
+# *Firmware* 173'28''
+# 
+# Software that's permanently stored on a computer component.
+# 
+# ### Interacting with Software
+# 
+# **Managing Software** 178'53''
+# 
+# *Git* 181'46''
+# 
+# A version control system版本控制系统 that helps keep track pf changes made to files and directories.
+# 
+# **software automation** 184'54''
+# 
+# ## Troubleshooting
+# ### Troubleshooting Best Practice
+#  
+# *Troubleshooting*
+# 
+# The ability to diagnose and resolve a problem.
+# 
+# 
+# 
+#  
+#  
+# 
+# 
+# 
+#  
+# 
+# 
+
+# 
